340aaa34a9716400fcd623465ce5d8a2f9010a09
Michael Harris <mhguitar1997@gmail.com>
2019-08-09 08:23:42 -0700

    Rmove quanity from abstract coin class

--- src/piggyBank/Coin.java
+++ src/piggyBank/Coin.java
@@ -3,12 +3,10 @@ package piggyBank;
 public abstract class Coin {
     protected String name;
     protected double value;
-    protected int quanity = 1;
-    
 
-    public Coin(String name, double value, int quanity){
+
+    public Coin(String name, double value){
         this.name = name;
         this.value = value;
-        this.quanity = quanity;
     }
 }
