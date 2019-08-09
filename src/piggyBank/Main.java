3b04f45fa6a2f0397eaea5722581bf787e2afab3
Michael Harris <mhguitar1997@gmail.com>
2019-08-09 08:46:49 -0700

    Coins created and can accept quanity or default 1

--- src/piggyBank/Main.java
+++ src/piggyBank/Main.java
@@ -4,8 +4,17 @@ import java.util.*;
 
 public class Main {
     private static void createCoins() {
-        Coin penny = new Coin("Penny", .01);
+        Coin penny = new Coin("Penny", .01, 12);
+        Coin nickel = new Coin("Nickel", .05);
+        Coin dime = new Coin("Dime", .10);
+        Coin quarter = new Coin("Quarter", .25, 3);
+        Coin dollar = new Coin("Dollar", 1.00, 7);
         System.out.println(penny);
+        System.out.println(nickel);
+        System.out.println(dime);
+        System.out.println(quarter);
+        System.out.println(dollar);
+
     }
     public static void main(String[] args){
         createCoins();
