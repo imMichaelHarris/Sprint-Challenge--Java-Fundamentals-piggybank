fbdf893f41317b8fa0efe239d9762c1005b5201b
Michael Harris <mhguitar1997@gmail.com>
2019-08-09 08:21:48 -0700

    Add abstract constructor for a coin that can accept quanity as well

--- src/piggyBank/Coin.java
+++ src/piggyBank/Coin.java
@@ -3,4 +3,12 @@ package piggyBank;
 public abstract class Coin {
     protected String name;
     protected double value;
+    protected int quanity = 1;
+    
+
+    public Coin(String name, double value, int quanity){
+        this.name = name;
+        this.value = value;
+        this.quanity = quanity;
+    }
 }
