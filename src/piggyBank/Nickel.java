8b26f2c6ebe9b4dfa48522de5b6b7a7b58efd874
Michael Harris <mhguitar1997@gmail.com>
2019-08-09 09:31:47 -0700

    Nickel done

--- src/piggyBank/Nickel.java
+++ src/piggyBank/Nickel.java
@@ -1,5 +1,21 @@
 package piggyBank;
 
 public class Nickel extends Coin{
+    private String name = "Nickel";
+    private double value = .05;
+
     
+    public Nickel(int quanity){
+        super(quanity);
+    }
+
+    @Override 
+    public double total(){
+        return value * getQuanity();
+    }
+
+    @Override 
+    public double getValue(){
+        return value;
+    }
 }
